60abba379d934228b3bc30f5a4897b8fbc08e475
alwynchan1679 <alwynchan1679@gmail.com>
2025-11-11 13:17:22 +0000

    Add files via upload

---
+++ swiftbot_simon.java
@@ -0,0 +1,13 @@
+import swiftbot.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Scanner;
+
+public class swiftbot_simon {
+	static SwiftBotAPI swiftBot;
+	
+	// Start of the program
+	
+}
